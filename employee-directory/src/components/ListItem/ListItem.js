e324653dcdd96b62e5ec856e2c4844ea594fa840
Alicia Breyer <alicia_breyer@yahoo.com>
2021-06-19 13:18:01 -0700

    added ListItem code

---
+++ employee-directory/src/components/ListItem/ListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ListItem({ user, index }) {
+  return (
+    <tr>
+      <td>
+        <b>{index}</b>
+      </td>
+      <td>
+        <img src={user.image} alt={user.firstname} />
+      </td>
+      <td>{user.name}</td>
+      <td>
+        <a href={`mailto:${user.email}`}>{user.email}</a>
+      </td>
+    </tr>
+  );
+}
+
+export default ListItem;
