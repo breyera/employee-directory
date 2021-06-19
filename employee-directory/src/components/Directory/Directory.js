32807848d60f741cc9cea59ad327a3b15a5221dd
Alicia Breyer <alicia_breyer@yahoo.com>
2021-06-19 13:12:30 -0700

    added files to component folders

---
+++ employee-directory/src/components/Directory/Directory.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ListItem from "../../components/ListItem";
+
+function Directory(props) {
+  const users = props.search ? props.sortlist : props.user;
+  return (
+    <div className="container-fluid">
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Photo</th>
+            <th scope="col">
+              <a
+                className="clickable"
+                onClick={(e) => {
+                  props.filter(e);
+                }}
+              >
+                Name <span className={props.toggle}></span>
+              </a>
+            </th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, i) => (
+            <ListItem key={i} user={user} index={i + 1} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Directory;
