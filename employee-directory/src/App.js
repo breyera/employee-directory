79b1331ea0084fd26369946af3cfae051a0fa287
Alicia Breyer <alicia_breyer@yahoo.com>
2021-06-19 13:35:06 -0700

    added app.js code for main component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -17,6 +17,42 @@ class App extends React.Component {
       .then((res) => this.setState({ users: res }))
       .catch((err) => console.log(err));
   };
+
+  filter = (e) => {
+    let sortList = this.state.users;
+    let toggle = this.state.toggle;
+    if (e.target.value) {
+      sortList = sortList.filter((user) =>
+        user.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      this.setState({ search: e.target.value });
+    } else {
+      sortList = sortList.reverse();
+      toggle === "dn"
+        ? this.setState({ toggle: up })
+        : this.setState({ toggle: "dn " });
+    }
+    this.setState({ sortList: sortList });
+  };
+
+  render() {
+    const { users, sortList, toggle, search } = this.state;
+    return (
+      <div>
+        <Navbar filter={this.filter} />
+        <main>
+          <Directory
+            filter={this.filter}
+            users={users}
+            sortList={sortList}
+            toggle={toggle}
+            search={search}
+          />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
